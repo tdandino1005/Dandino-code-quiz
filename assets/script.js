d0e42d82bc25c0be99d017648e8be48632c204b9
Tony Dandino <tdandino1005@gmail.com>
2022-11-07 08:16:00 -0600

    made changes to JS

--- assets/script.js
+++ assets/script.js
@@ -1,55 +1,46 @@
-// variables for page elements
-// time and score
 let timeEl = document.querySelector("p.time");
 let secondsLeft = 90;
 let scoreEl = document.querySelector("#score");
 
-// sections
-// section intro
 const introEl = document.querySelector("#intro");
 
-// section questions
-//question section
 const questionsEl = document.querySelector("#questions");
-//where question goes
 let questionEl = document.querySelector("#question");
-// how many questions they have answered
+
 let questionCount = 0;
-// div yaynay
+
 const yaynayEl = document.querySelector("#yaynay");
 
-// section final
+
 const finalEl = document.querySelector("#final");
-// user initials
+
 let initialsInput = document.querySelector("#initials");
 
-// section highscores
+
 const highscoresEl = document.querySelector("#highscores");
-// ordered list
+
 let scoreListEl = document.querySelector("#score-list");
-// array of scores
+
 let scoreList = [];
 
-// buttons
-// start
 const startBtn = document.querySelector("#start");
-// answer button class
+
 const ansBtn = document.querySelectorAll("button.ansBtn")
-// answer1
+
 const ans1Btn = document.querySelector("#answer1");
-// answer2
+
 const ans2Btn = document.querySelector("#answer2");
-// answer3
+
 const ans3Btn = document.querySelector("#answer3");
-// answer4
+
 const ans4Btn = document.querySelector("#answer4");
-// submit-score
+
 const submitScrBtn = document.querySelector("#submit-score");
-// goback
+
 const goBackBtn = document.querySelector("#goback");
-// clearscores
+
 const clearScrBtn = document.querySelector("#clearscores");
-// view-scores
+
 const viewScrBtn = document.querySelector("#view-scores");
 
 const questions = [ // array of objects
@@ -86,9 +77,6 @@ const questions = [ // array of objects
 ];
 
 
-// Functions
-
-// timer
 function setTime() {
     let timerInterval = setInterval(function () {
         secondsLeft--;
@@ -103,7 +91,6 @@ function setTime() {
     }, 1000);
 }
 
-// start quiz with timer and set up questions
 function startQuiz() {
     introEl.style.display = "none";
     questionsEl.style.display = "block";
@@ -113,7 +100,6 @@ function startQuiz() {
     setQuestion(questionCount);
 }
 
-// function to set question; takes in a count and displays the next question/answers
 function setQuestion(id) {
     if (id < questions.length) {
         questionEl.textContent = questions[id].question;
@@ -124,21 +110,19 @@ function setQuestion(id) {
     }
 }
 
-// function to check answer and then move to next question
 function checkAnswer(event) {
     event.preventDefault();
 
-    // show section for yaynay and append message
     yaynayEl.style.display = "block";
     let p = document.createElement("p");
     yaynayEl.appendChild(p);
 
-    // time out after 1 second
+
     setTimeout(function () {
         p.style.display = 'none';
     }, 1000);
 
-    // answer checker
+
     if (questions[questionCount].correctAnswer === event.target.value) {
         p.textContent = "Correct!";
     } else if (questions[questionCount].correctAnswer !== event.target.value) {
@@ -146,11 +130,10 @@ function checkAnswer(event) {
         p.textContent = "Wrong!";
     }
 
-    // increment so the questions index is increased
     if (questionCount < questions.length) {
         questionCount++;
     }
-    // call setQuestion to bring in next question when any ansBtn is clicked
+
     setQuestion(questionCount);
 }
 
@@ -163,7 +146,6 @@ function addScore(event) {
     let init = initialsInput.value.toUpperCase();
     scoreList.push({ initials: init, score: secondsLeft });
 
-    // sort scores
     scoreList = scoreList.sort((a, b) => {
         if (a.score < b.score) {
           return 1;
@@ -179,7 +161,6 @@ function addScore(event) {
         scoreListEl.append(li);
     }
 
-    // Add to local storage
     storeScores();
     displayScores();
 }
@@ -189,35 +170,27 @@ function storeScores() {
 }
 
 function displayScores() {
-    // Get stored scores from localStorage
-    // Parsing the JSON string to an object
     let storedScoreList = JSON.parse(localStorage.getItem("scoreList"));
 
-    // If scores were retrieved from localStorage, update the scorelist array to it
     if (storedScoreList !== null) {
         scoreList = storedScoreList;
     }
 }
 
-// clear scores
 function clearScores() {
     localStorage.clear();
     scoreListEl.innerHTML="";
 }
 
-// EventListeners
-// Start timer and display first question when click start quiz
 startBtn.addEventListener("click", startQuiz);
 
-// Check answers loop
 ansBtn.forEach(item => {
     item.addEventListener('click', checkAnswer);
 });
 
-// Add score
 submitScrBtn.addEventListener("click", addScore);
 
-// Go Back Button
+
 goBackBtn.addEventListener("click", function () {
     highscoresEl.style.display = "none";
     introEl.style.display = "block";
@@ -225,10 +198,8 @@ goBackBtn.addEventListener("click", function () {
     timeEl.textContent = `Time:${secondsLeft}s`;
 });
 
-// Clear the scores
 clearScrBtn.addEventListener("click", clearScores);
 
-// View/Hide High Scores Button
 viewScrBtn.addEventListener("click", function () {
     if (highscoresEl.style.display === "none") {
         highscoresEl.style.display = "block";
