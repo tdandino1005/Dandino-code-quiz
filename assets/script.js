d2ea8f75505e49acf7e85b26d711ae64ef5a1538
Tony Dandino <tdandino1005@gmail.com>
2022-11-06 21:41:45 -0600

    added questions

--- assets/script.js
+++ assets/script.js
@@ -55,33 +55,33 @@ const viewScrBtn = document.querySelector("#view-scores");
 // Object for question, answer, true/false
 const questions = [ // array of objects
     {
-        // question 0
+        
         question: "Commonly used data types do NOT include:",
         answers: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
         correctAnswer: "2"
     },
     {
-        // question 1
+       
         question: "The condition in an if / else statement is enclosed within ____.",
         answers: ["1. quotes", "2. curly brackets", "3. parentheses", "4. square brackets"],
         correctAnswer: "1"
     },
     {
-        // question 2
+        
         question: "Arrays in Javascript can be used to store ____.",
         answers: ["1. numbers and strings", "2. other arrays", "3. booleans", "4. all of the above"],
         correctAnswer: "3"
     },
     {
-        // question 3
+        
         question: "String values must be enclosed within ____ when being assigned to variables.",
         answers: ["1. commmas", "2. curly brackets", "3. quotes", "4. parentheses"],
         correctAnswer: "2"
     },
     {
-        // question 4
-        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
-        answers: ["1. Javascript", "2. terminal/bash", "3. for loops", "4. console.log"],
+        
+        question: "Inside which HTML element do we put the JavaScript?",
+        choices: ["a. <js>", "b. <javascript>", "c. <scripting>", "d. <script>"],
         correctAnswer: "3"
     }
 ];
